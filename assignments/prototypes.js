79f2ef8c40535bc7ccbe4b56b962c9cec9b76448
DimejiAre <olardi2@gmail.com>
2019-08-21 14:33:31 +0100

    complete stretch tasks in prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -194,12 +194,30 @@ Shirt.prototype.mend = function(){
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(data){
+  this.createdAt = data.createdAt;
+  this.name = data.name;
+  this.dimensions = data.dimensions;
+}
+GameObject.prototype.destroy = function (){
+  return `${this.name} was removed from the game.`;
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(data){
+  GameObject.call(this,data);
+  this.healthPoints = data.healthPoints;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -211,6 +229,17 @@ Shirt.prototype.mend = function(){
   * should inherit takeDamage() from CharacterStats
 */
 
+function Humanoid(data){
+  CharacterStats.call(this,data)
+  this.team = data.team;
+  this.weapons = data.weapons;
+  this.language = data.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -219,7 +248,6 @@ Shirt.prototype.mend = function(){
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -277,4 +305,3 @@ Shirt.prototype.mend = function(){
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
